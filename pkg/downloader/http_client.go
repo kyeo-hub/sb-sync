806f728c5986c7ffeba67a21ba28d40cdf3c2496
kyeo-hub <kyeo-hub@users.noreply.github.com>
2026-05-09 05:39:59 +0000

    fix: 添加 HTTP 代理环境变量支持，解决 update 卡住问题

--- pkg/downloader/http_client.go
+++ pkg/downloader/http_client.go
@@ -2,6 +2,8 @@ package downloader
 
 import (
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 
 	"github.com/go-resty/resty/v2"
@@ -14,16 +16,32 @@ var (
 
 func getHTTPClient() *resty.Client {
 	if !httpClientOnce {
-		httpClient = resty.New().
+		client := resty.New().
 			SetTimeout(5 * time.Minute).
 			SetRetryCount(3).
 			SetRetryWaitTime(5 * time.Second).
-			SetRetryMaxWaitTime(30 * time.Second).
-			SetTransport(&http.Transport{
-				MaxIdleConns:        10,
-				MaxIdleConnsPerHost: 5,
-				IdleConnTimeout:     30 * time.Second,
-			})
+			SetRetryMaxWaitTime(30 * time.Second)
+
+		proxyURL := os.Getenv("HTTPS_PROXY")
+		if proxyURL == "" {
+			proxyURL = os.Getenv("https_proxy")
+		}
+		if proxyURL == "" {
+			proxyURL = os.Getenv("HTTP_PROXY")
+		}
+		if proxyURL == "" {
+			proxyURL = os.Getenv("http_proxy")
+		}
+
+		if proxyURL != "" {
+			if parsedURL, err := url.Parse(proxyURL); err == nil {
+				client.SetTransport(&http.Transport{
+					Proxy: http.ProxyURL(parsedURL),
+				})
+			}
+		}
+
+		httpClient = client
 		httpClientOnce = true
 	}
 	return httpClient
